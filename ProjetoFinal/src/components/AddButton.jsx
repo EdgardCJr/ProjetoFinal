7f31dbac7f537044999472a4ab1f31c11ebd4297
EdgardCJr <edgardcastanhojr@hotmail.com>
2025-03-01 20:25:44 -0300

    Alterações finais

---
+++ ProjetoFinal/src/components/AddButton.jsx
@@ -0,0 +1,33 @@
+import Plus from "../icons/Plus";
+import colors from "../assets/colors";
+import { useRef } from "react";
+import { db } from "../appwrite/databases";
+import { useContext } from "react";
+import { NotesContext } from "../context/NotesContext";
+ 
+const AddButton = () => {
+    const { setNotes } = useContext(NotesContext);
+    const startingPos = useRef(10);
+
+    const AddNote = async () => {
+        const payload = {  
+            position: JSON.stringify({ 
+                x: startingPos.current,
+                y: startingPos.current 
+            }),
+            colors: JSON.stringify(colors[0]),
+        };
+
+        startingPos.current += 10;
+        const response = await db.notes.create(payload);
+        setNotes((prevState) => [response, ...prevState]);
+    };
+
+    return (
+        <div id="add-btn" onClick={AddNote}>
+            <Plus />
+        </div>
+    );
+};
+
+export default AddButton;
---
+++ ProjetoFinal/src/pages/NotesPage.jsx
@@ -0,0 +1,19 @@
+import NoteCard from "../pages/NoteCard";
+import Controls from "../components/Controls";
+import { useContext } from "react";
+import { NotesContext } from "../context/NotesContext";
+
+const NotesPage = () => {
+    const { notes } = useContext(NotesContext);
+
+    return (
+        <div>
+            {notes.map((note) => (
+                <NoteCard key={note.$id} note={note}/>
+            ))}
+            <Controls />
+        </div>
+    );
+};
+
+export default NotesPage;
